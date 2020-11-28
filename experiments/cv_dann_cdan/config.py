1ca88d7cb5d5052516376f754a7fb929b162d2c6
Shuo Zhou <szhou20@sheffield.ac.uk>
2020-11-28 23:59:10 +0800

    Create config.py

--- experiments/cv_dann_cdan/config.py
+++ experiments/cv_dann_cdan/config.py
@@ -3,7 +3,7 @@ Default configurations for domain adapation
 """
 
 from yacs.config import CfgNode as CN
-import os
+# import os
 
 # -----------------------------------------------------------------------------
 # Config definition
@@ -15,11 +15,13 @@ _C = CN()
 # Dataset
 # -----------------------------------------------------------------------------
 _C.DATASET = CN()
-_C.DATASET.ROOT = '../data'  # '/shared/tale2/Shared'
-_C.DATASET.NAME = 'digits'  # dset choices=['office', 'image-clef', 'office-home']
-_C.DATASET.SOURCE = 'mnist'  # s_dset_path  , help="The source dataset path list"
-_C.DATASET.TARGET = 'usps'  # s_dset_path  , help="The target dataset path list"
-_C.DATASET.NUM_CLASSES = 10
+_C.DATASET.ROOT = '/shared/tale2/Shared/data/PACS/raw'  # 'D:/ML_data/PACS/kfold'  # '../data'  # '/shared/tale2/Shared'
+_C.DATASET.NAME = 'pacs'  # 'digits'  # dset choices=['office', 'image-clef', 'office-home']
+# _C.DATASET.SOURCE = 'mnist'  # s_dset_path  , help="The source dataset path list"
+# _C.DATASET.TARGET = 'usps'  # s_dset_path  , help="The target dataset path list"
+_C.DATASET.SOURCE = ['cartoon', 'art_painting', 'photo']
+_C.DATASET.TARGET = ['sketch']
+_C.DATASET.NUM_CLASSES = 7
 _C.DATASET.NUM_REPEAT = 10  # 10
 _C.DATASET.DIMENSION = 784
 _C.DATASET.WEIGHT_TYPE = 'natural'
@@ -57,11 +59,10 @@ _C.DAN.RANDOM_DIM = 1024
 # Misc options
 # ---------------------------------------------------------------------------- #
 _C.OUTPUT = CN()
-_C.OUTPUT.ROOT = './outputs'  # output_dir
+_C.OUTPUT.ROOT = '/shared/tale2/Shared/szhou/results/pykale/outputs'  # './outputs'  # output_dir
 _C.OUTPUT.VERBOSE = False  # To discuss, for HPC jobs
 _C.OUTPUT.PB_FRESH = 0  # 0 # 50 # 0 to disable  ; MAYBE make it a command line option
-_C.OUTPUT.DIR = os.path.join(_C.OUTPUT.ROOT, _C.DATASET.NAME + '_' +
-                             _C.DATASET.SOURCE + '2' + _C.DATASET.TARGET)
+# _C.OUTPUT.DIR = os.path.join(_C.OUTPUT.ROOT, _C.DATASET.NAME + '_rest2' + _C.DATASET.TARGET[0])
 
 
 def get_cfg_defaults():
